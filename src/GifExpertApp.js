0bdfc823bcad3bc82584658c04e1cc55aac58e24
Oscar Terceros <osterce@gmail.com>
2021-11-30 12:27:58 -0400

    Renderizar los li de categorias y agregar una nueva a la lista

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import { useState } from "react";
 
-const GifExpertApp = () => {
+export const GifExpertApp = () => {
+  
+  const [categories, setCategories] = useState( ['One Punch', 'Samurai X', 'Dragon Ball'] );
+
+  let nuevoAnime = 'Hunter X Hunter';
+
+  const handleAdd = () => setCategories( categories => [ nuevoAnime, ...categories ] );
+
   return (
     <>
-     <h2>GifExpertApp</h2>
-     <hr />
+      <h2>GifExpertApp</h2>
+      <hr />
+
+      <button onClick = { handleAdd } >Agregar</button>
+
+      <ol>
+       {
+         categories.map( category => {
+            return <li key = { category } > { category } </li>
+         })
+       }
+      </ol>
     </>
   )
 }
-
-export default GifExpertApp
