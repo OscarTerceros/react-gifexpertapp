6c3933aebd8e6262ffc14eafec9a6200a00751b7
Oscar Terceros <osterce@gmail.com>
2021-12-01 22:06:36 -0400

    Modificaciones Custom Hook useFetchGifs.js -> Obtener imágenes y loading

--- src/components/GiftGrid.js
+++ src/components/GiftGrid.js
@@ -1,39 +1,31 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs';
-//import { getGifs } from '../helpers/getGifs';
-//import { GifGridItem } from './GifGridItem';
+
+import { GifGridItem } from './GifGridItem';
 
 export const GiftGrid = ( { category } ) => {
   
-  const { loading } = useFetchGifs();
+  const { data:images, loading } = useFetchGifs( category );
 
-
-
-  /* const [images, setImages] = useState([]);
-  
-  useEffect( () => {
-    getGifs( category )
-      .then( imgs => setImages( imgs ))
-  }, [ category ]) */
-  
   return (
     <>
       <h3> { category } </h3>
 
-      { loading ? 'Cargando...' : 'Data Cargada' }
+      { loading && <p>Cargando...</p> }
 
-      {/* <div className = "card-grid">
+      <div className = "card-grid">
         
-        { images.map( ( img ) => 
-          (
-            <GifGridItem 
-              key = { img.id }
-              { ...img }
-            />
-          ))
+        { 
+          images .map( ( img ) => 
+            (
+              <GifGridItem 
+                key = { img.id }
+                { ...img }
+              />
+            ))
         }
         
-      </div> */}
+      </div>
     </>
   )
 }
